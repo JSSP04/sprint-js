933222cf1b552e204ee4a97d20f536e2dc6e34d8
Borsato <joao.pborsato@gmail.com>
2023-11-12 08:03:52 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Header from './componentes/Header'
 import Footer from './Footer'
 import Body from './componentes/Body'
-import Cadastro from './componentes/Cadastro';
-
 
 function App() {
 
@@ -16,7 +14,6 @@ function App() {
     <div>
       <Header />
       <Body />
-      <Cadastro />
       <Footer />
     </div>
 
--- src/componentes/Body.jsx
+++ src/componentes/Body.jsx
@@ -1,6 +1,36 @@
 import '../assets/scss/styles.scss';
 import '../assets/css/styles.css';
 
+function Enviar() {
+  // Captura dos valores dos campos
+  const nome = document.getElementById('nome').value;
+  const email = document.getElementById('email').value;
+  const telefone = document.getElementById('telefone').value;
+  const cep = document.getElementById('cep').value;
+  const cidade = document.getElementById('cidade').value;
+  const estado = document.getElementById('estado').value;
+
+  const dados = {
+    nome,
+    email,
+    telefone,
+    cep,
+    cidade,
+    estado
+  };
+
+  localStorage.setItem('formularioData', JSON.stringify(dados));
+
+  alert('Dados salvos com sucesso!');
+
+  document.getElementById('nome').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('telefone').value = '';
+  document.getElementById('cep').value = '';
+  document.getElementById('cidade').value = '';
+  document.getElementById('estado').value = '';
+}
+
 
 function Body () {
 return (
@@ -51,7 +81,80 @@ return (
         <p>Confiança é a base de tudo. Na SafeFlood, cada alerta é mais do que uma notificação; é a certeza de que você está preparado. Explore a confiança que construímos, baseada em dados precisos, informações confiáveis e um compromisso inabalável com a sua segurança</p>
       </div>
     </div>
-  </div> 
+  </div>
+  <div id="Cadastro" className="cadastro">
+        <div className="bgt">
+            
+        </div>
+        <div className="text2"><h2>Cadastre-se para baixar o nosso APP!</h2></div>
+        <div className="text3"><h3>Disponível nas plataformas IOS e Android</h3></div>
+        <div className="box2"></div>
+        <div className="button1">
+
+            <div className="d-grid gap-2 col-6 mx-auto p-5">
+                <button type="button" className="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    Cadastre-se
+                </button>
+            </div>
+
+            <div className="modal fade " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered modal-lg">
+                    <div className="modal-content">
+                        <div className="modal-header" data-bs-theme="dark">
+                            <h1 className="modal-title fs-5" id="staticBackdropLabel">Cadastro para o APP Safe Flood</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form className="row mb-4 p-3 justify-content-md-evenly bg-7">
+                                <div className="col-md-5 my-2">
+                                    <label htmlFor="nome" className="form-label">Nome</label>
+                                    <input type="text" className="form-control" id="nome" name="nome" placeholder="Ex: João da Silva" required/>
+                                </div>
+                                
+                                <div className="col-md-5 my-2">
+                                    <label htmlFor="email" className="form-label">Email</label>
+                                    <input type="email" className="form-control" id="email" name="email" placeholder="Ex: email@gmail.com" required/>
+                                </div>
+                            
+                                <div className="col-md-5 my-2">
+                                    <label htmlFor="telefone" className="form-label">Telefone</label>
+                                    <input type="tel" className="form-control" id="telefone" name="telefone" placeholder="Ex: 99999-9999" required/>
+                                </div>
+                            
+                                <div className="col-md-5 my-2">
+                                    <label htmlFor="cep" className="form-label">CEP</label>
+                                    <input type="text" className="form-control" id="cep" name="cep" placeholder="Ex: 11111-111" required/>
+                                </div>
+                            
+                                <div className="col-md-5 my-2">
+                                    <label htmlFor="cidade" className="form-label">Cidade</label>
+                                    <input type="text" className="form-control" id="cidade" name="cidade" placeholder="Ex: São Paulo" required/>
+                                </div>
+                            
+                                <div className="col-md-5 my-2">
+                                    <label htmlFor="estado" className="form-label">Estado</label>
+                                    <select className="form-select" id="estado" name="estado" required>
+                                        <option value="" disabled>Selecione o estado</option>
+                                        <option value="SP">São Paulo</option>
+                                        <option value="RJ">Rio de Janeiro</option>
+                                        <option value="DF">Distrito Federal</option>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer ">
+                            <div className="mx-auto">
+                                <button type="button" className="btn btn-danger btn-lg" data-bs-dismiss="modal">Fechar</button>
+                                <button type="button" className="btn btn-success btn-lg" onClick={Enviar}>Enviar</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </body>
 
 );
